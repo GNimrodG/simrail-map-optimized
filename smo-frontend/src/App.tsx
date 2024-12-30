151a66002014a823d3bd8b300f193af40df92ab9
Nimród Glöckl <g.nimrod.g@data-unknown.com>
2024-12-30 23:12:02 +0100

    Store selected train in URL hash (#108)

--- smo-frontend/src/App.tsx
+++ smo-frontend/src/App.tsx
@@ -9,23 +9,17 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import Loading from "./components/Loading";
 import MapLinesContext, { MapLineData } from "./utils/map-lines-context";
 import SelectedRouteContext from "./utils/selected-route-context";
-import SelectedTrainContext, { SelectedTrainData } from "./utils/selected-train-context";
+import SelectedTrainProvider from "./components/SelectedTrainProvider";
 
 const MainMap = lazy(() => import("./components/MainMap"));
 
 function App() {
   const [selectedRoute, setSelectedRoute] = useState<string | null>(null);
-  const [selectedTrain, setSelectedTrain] = useState<SelectedTrainData | null>(null);
   const [mapLines, setMapLines] = useState<MapLineData | null>(null);
 
   const selectedRouteContextValue = useMemo(
     () => ({ selectedRoute, setSelectedRoute }),
     [selectedRoute, setSelectedRoute],
-  );
-
-  const selectedTrainContextValue = useMemo(
-    () => ({ selectedTrain, setSelectedTrain }),
-    [selectedTrain, setSelectedTrain],
   );
 
   const mapLinesContextValue = useMemo(() => ({ mapLines, setMapLines }), [mapLines, setMapLines]);
@@ -34,7 +28,7 @@ function App() {
     <CssVarsProvider colorSchemeStorageKey="color-scheme" defaultMode="system">
       <CssBaseline />
       <SelectedRouteContext.Provider value={selectedRouteContextValue}>
-        <SelectedTrainContext.Provider value={selectedTrainContextValue}>
+        <SelectedTrainProvider>
           <MapLinesContext.Provider value={mapLinesContextValue}>
             <Suspense fallback={<Loading color="success" />}>
               <ErrorBoundary location="App">
@@ -42,7 +36,7 @@ function App() {
               </ErrorBoundary>
             </Suspense>
           </MapLinesContext.Provider>
-        </SelectedTrainContext.Provider>
+        </SelectedTrainProvider>
       </SelectedRouteContext.Provider>
     </CssVarsProvider>
   );
