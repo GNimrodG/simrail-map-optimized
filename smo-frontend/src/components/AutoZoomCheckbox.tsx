0c188081880e7e01c12525d40feffbe7d1a216ce
Nimród Glöckl <g.nimrod.g@data-unknown.com>
2024-12-31 00:02:29 +0100

    Build fix

--- smo-frontend/src/components/AutoZoomCheckbox.tsx
+++ smo-frontend/src/components/AutoZoomCheckbox.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
-import { useMap } from "react-leaflet";
-import { useTranslation } from "react-i18next";
 import Checkbox from "@mui/joy/Checkbox";
-import { useSetting } from "../utils/use-setting";
-import { signalsData$, trainsData$, stationsData$ } from "../utils/data-manager";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useMap } from "react-leaflet";
+
+import { signalsData$, stationsData$, trainsData$ } from "../utils/data-manager";
 import useBehaviorSubj from "../utils/use-behaviorSubj";
+import { useSetting } from "../utils/use-setting";
 
 const AutoZoomCheckbox = () => {
   const { t } = useTranslation("translation", { keyPrefix: "Settings" });
@@ -21,20 +22,16 @@ const AutoZoomCheckbox = () => {
     const handleZoom = () => {
       const bounds = map.getBounds();
       const visibleTrains = trains.filter((train) =>
-        bounds.contains([train.TrainData.Latititute, train.TrainData.Longitute])
+        bounds.contains([train.TrainData.Latititute, train.TrainData.Longitute]),
       );
-      const visibleSignals = signals.filter((signal) =>
-        bounds.contains([signal.lat, signal.lon])
-      );
-      const visibleStations = stations.filter((station) =>
-        bounds.contains([station.lat, station.lon])
-      );
+      const visibleSignals = signals.filter((signal) => bounds.contains([signal.lat, signal.lon]));
+      const visibleStations = stations.filter((station) => bounds.contains([station.Latititude, station.Longitude]));
 
       const totalVisibleObjects = visibleTrains.length + visibleSignals.length + visibleStations.length;
 
-      if (totalVisibleObjects < 10) {
+      if (totalVisibleObjects < 50) {
         map.zoomOut();
-      } else if (totalVisibleObjects > 20) {
+      } else if (totalVisibleObjects > 80) {
         map.zoomIn();
       }
     };
@@ -45,13 +42,7 @@ const AutoZoomCheckbox = () => {
     };
   }, [autoZoom, map, signals, stations, trains]);
 
-  return (
-    <Checkbox
-      label={t("autoZoom.Label")}
-      checked={autoZoom}
-      onChange={(e) => setAutoZoom(e.target.checked)}
-    />
-  );
+  return <Checkbox label={t("autoZoom.Label")} checked={autoZoom} onChange={(e) => setAutoZoom(e.target.checked)} />;
 };
 
 export default AutoZoomCheckbox;
