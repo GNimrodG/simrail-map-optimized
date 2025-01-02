311e4661c00e0d5eff21b7035317a6d5a9f809c8
Nimród Glöckl <g.nimrod.g@data-unknown.com>
2025-01-02 14:17:56 +0100

    Added PWA to preload assets and handle updates

--- smo-frontend/vite.config.ts
+++ smo-frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -12,6 +13,20 @@ export default defineConfig({
       project: "smo",
     }),
     svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "SMO",
+        short_name: "SMO",
+        categories: ["games", "utilities", "maps", "transportation"],
+        description: "An optimized and feature rich online map for the game Simrail",
+        background_color: "#0b0d0e",
+        theme_color: "#185ea5",
+      },
+    }),
   ],
 
   build: {
