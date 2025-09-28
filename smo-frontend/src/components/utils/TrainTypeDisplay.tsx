920d1f020c5f07351ff0c2ab6bd9506ed74bcb10
AMelonInsideLemon <166175391+AMelonInsideLemon@users.noreply.github.com>
2025-09-29 01:24:59 +0200

    Simplify and update train type translations
    Updated train types for EN/PL according to Annex 6.3 of the 2024/2025 Network Statement. Description of the ZX type is taken from Annex 6.1 of the 2018/2019 Network Statement, which was the last version containing it.

--- smo-frontend/src/components/utils/TrainTypeDisplay.tsx
+++ smo-frontend/src/components/utils/TrainTypeDisplay.tsx
@@ -13,15 +13,39 @@ export interface TrainTypeDisplayProps {
 const TrainTypeDisplay: FunctionComponent<TrainTypeDisplayProps> = ({ type }) => {
   const { t, i18n } = useTranslation("translation", { keyPrefix: "TrainTypes" });
 
-  const actualType = type.replace(/^([A-Z]{3}) - ".+/g, "$1");
+  let title: string | undefined;
+
+  if (i18n.exists(`TrainTypes.Special.${type}`)) {
+    title = t(`TrainTypes.Special.${type}`);
+  } else {
+    const [main, mod] = [type.slice(0, 2), type[2]];
+
+    let mainKey = `Main.${main}`;
+    let modKey = `Modifiers.${mod}`;
+    let formatKey = "Format";
+
+    if (!i18n.exists(`TrainTypes.${mainKey}`)) {
+      mainKey = `Locomotive.${main}`;
+      modKey = `LocomotiveModifires.${mod}`;
+      formatKey = "LocomotiveFormat";
+    }
+
+    const mainText = i18n.exists(`TrainTypes.${mainKey}`) ? t(mainKey) : "";
+    const modText = mod && i18n.exists(`TrainTypes.${modKey}`) ? t(modKey) : "";
+
+    if (mainText) {
+      const format = i18n.exists(`TrainTypes.${formatKey}`) ? t(formatKey) : "{main} {mod}";
+      title = format.replace("{main}", mainText).replace("{mod}", modText).trim();
+    }
+  }
 
   return (
     <Stack direction="row" spacing={1} alignItems="center">
       <Box component="span" sx={{ textWrap: "nowrap" }}>
         {type}
       </Box>
-      {i18n.exists(`TrainTypes.${actualType}`) && (
-        <Tooltip arrow variant="outlined" placement="right" describeChild title={t(actualType)}>
+      {title && (
+        <Tooltip arrow variant="outlined" placement="right" describeChild title={title}>
           <Stack alignItems="center" justifyContent="center">
             <InfoIcon />
           </Stack>
